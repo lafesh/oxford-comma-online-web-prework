db5bbf7d78810523c4da577a7812239df3f4e954
Gina Fedder <ginafedder@yahoo.com>
2018-09-17 22:04:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,10 @@ def oxford_comma(array)
   if array.length == 2 
     array.join(" and ")
   elsif array.length == 3
-    array.join(" , " , " and ")
+    array[-2] << "and"
+    array.join(" , ")
+  elsif array.length > 3
+    array.join(" ")
   else
     array.join
   end
